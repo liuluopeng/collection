ea90fc7a6264d03aa7efaf3ec6653bc7bcba83f9
liuluopeng <liuluopeng@foxmail.com>
2023-08-30 23:41:33 +0800

    insert sort

--- 003_ppsort/tests/test_insert_sort.rs
+++ 003_ppsort/tests/test_insert_sort.rs
@@ -1,34 +1,31 @@
-mod helper;
-mod insert_sort;
-mod merge_sort;
-mod quick_sort;
-pub use insert_sort::insert_sort;
-pub use merge_sort::merge_sort;
-pub use quick_sort::quick_sort;
+use ppsort::insert_sort;
 
 #[cfg(test)]
 mod tests {
     use rand::Rng;
 
     use super::*;
-  
+
     #[test]
-    fn test_merge_sort() {
+    fn test_insert_sort() {
         // 创建一个随机数生成器
         let mut rng = rand::thread_rng();
 
         let mut arr = Vec::with_capacity(1000000);
 
         // 生成随机数并将其添加到向量中
-        for _ in 0..1000000 {
+        for _ in 0..100 {
             let random_number = rng.gen_range(0..=100000);
             arr.push(random_number);
         }
 
-        merge_sort(&mut arr);
+        println!("{:?}", arr);
+        insert_sort(&mut arr);
+        println!("{:?}", arr);
 
         for i in 0..arr.len() - 1 {
             assert!(arr[i] <= arr[i + 1]);
         }
     }
+
 }
