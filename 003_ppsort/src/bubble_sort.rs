82473527757c1a8f3165ab90d2486f25c7007eae
liuluopeng <liuluopeng@foxmail.com>
2023-09-02 17:28:02 +0800

    bubble sort; selection sort

--- 003_ppsort/src/bubble_sort.rs
+++ 003_ppsort/src/bubble_sort.rs
@@ -1,38 +1,33 @@
-mod helper;
-mod insert_sort;
-mod merge_sort;
-mod quick_sort;
-mod counting_sort;
-mod selection_sort;
-mod bubble_sort;
-pub use insert_sort::insert_sort;
-pub use merge_sort::merge_sort;
-pub use quick_sort::quick_sort;
-pub use counting_sort::counting_sort;
-pub use selection_sort::selection_sort;
-pub use bubble_sort::bubble_sort;
+pub fn bubble_sort<T: Ord + Clone + std::fmt::Display + std::fmt::Debug>(arr: &mut [T]) {
+    let length = arr.len();
+    for _ in 0..length {
+        for j in 0..length-1 {
+            if arr[j] > arr[j+1] {
+                arr.swap(j, j+1);
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use rand::Rng;
 
-    use super::*;
-  
     #[test]
-    fn test_merge_sort() {
+    fn test_bubble_sort() {
         // 创建一个随机数生成器
         let mut rng = rand::thread_rng();
 
         let mut arr = Vec::with_capacity(1000000);
 
         // 生成随机数并将其添加到向量中
-        for _ in 0..1000000 {
+        for _ in 0..100 {
             let random_number = rng.gen_range(0..=100000);
             arr.push(random_number);
         }
-
-        merge_sort(&mut arr);
-
+        bubble_sort(&mut arr);
+        println!("{:?}", arr);
         for i in 0..arr.len() - 1 {
             assert!(arr[i] <= arr[i + 1]);
         }
